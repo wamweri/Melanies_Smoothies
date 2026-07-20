655a788f611e93ca2c99d7b2835d532627359ea7
wamweri <122532928+wamweri@users.noreply.github.com>
2026-07-20 19:35:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,4 +61,7 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.text(smoothiefroot_response)
+sf_df = st.dataframe(
+    data=smoothiefroot_response.json(),
+    use_container_width=True
+)
